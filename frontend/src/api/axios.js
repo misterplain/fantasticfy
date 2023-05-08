b392d674e8a262a8e84c1025210bff4a3e3550a3
Patrick O'Brien <obrienp2321@gmail.com>
2023-05-08 14:02:34 +0200

    route axios base URL to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-//   baseURL: "https://activeserver.onrender.com/fantasticfy/api",
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://activeserver.onrender.com/fantasticfy/data",
+  // baseURL: "http://localhost:5000/fantasticfy/data",
 });
