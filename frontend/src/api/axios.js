56be09997d08d0452d7a355e18e6b190033d8973
Patrick O'Brien <obrienp2321@gmail.com>
2023-05-23 12:57:38 +0200

    render to cyclic backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://activeserver.onrender.com/fantasticfy/data",
+  // baseURL: "https://activeserver.onrender.com/fantasticfy/data",
   // baseURL: "http://localhost:5000/fantasticfy/data",
+  baseURL: "https://cute-gray-pelican-tux.cyclic.app/fantasticfy/data"
 });
